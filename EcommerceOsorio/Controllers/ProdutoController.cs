a93e75f8e510584209fa547cba7a3b7faa451cbd
Anna <anna.nester@gmail.com>
2018-07-27 09:18:07 -0300

    Criação da Action e View de Cadastrar Produto

--- EcommerceOsorio/Controllers/ProdutoController.cs
+++ EcommerceOsorio/Controllers/ProdutoController.cs
@@ -9,5 +9,10 @@ namespace EcommerceOsorio.Controllers
         {
             return View();
         }
+
+        public ActionResult CadastrarProduto()
+        {
+            return View();
+        }
     }
 }
