f77717cb77362ef5d24b75e1ec12ca8ce38ca93d
Anna <anna.nester@gmail.com>
2018-07-27 09:01:04 -0300

    Criação de Index

---
+++ EcommerceOsorio/Controllers/ProdutoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace EcommerceOsorio.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
